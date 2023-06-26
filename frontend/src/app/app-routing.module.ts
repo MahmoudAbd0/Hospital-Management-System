7252deda6f32674b81790f64fa6570dcff97a8ba
aya-mustafa <ayamustafa408@gmail.com>
2023-06-26 21:58:54 +0300

    add admin dashboard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { DoctorComponent } from './components/doctor/doctor.component';
+import { ReceptionistComponent } from './components/receptionist/receptionist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"admin", component:AdminComponent},
+  {path:"doctor", component:DoctorComponent},
+  {path:"receptionist", component:ReceptionistComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { DoctorComponent } from './components/doctor/doctor.component';
+import { ReceptionistComponent } from './components/receptionist/receptionist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    AdminComponent,
+    DoctorComponent,
+    ReceptionistComponent
   ],
   imports: [
     BrowserModule,
