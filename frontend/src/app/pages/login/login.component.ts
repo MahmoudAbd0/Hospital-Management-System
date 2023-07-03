ac0fee81c6e908abce893569690ca3f2d717a8c2
Yehia Nabil <yehn@outlook.com>
2023-07-03 17:18:54 +0300

    updating login

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -2,19 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
 
   form!: FormGroup;
 
   constructor(private fb: FormBuilder, 
-              private http: HttpClient,
+              private loginService:LoginService,
               private router: Router) { }
 
   ngOnInit() {
@@ -27,21 +29,22 @@ export class LoginComponent implements OnInit {
   submit() {
     if (this.form.valid) {
       const formData = this.form.getRawValue();
+      const email = formData.email;
+      const password = formData.password;
 
-      const data = {
-        email: formData.email,
-        password: formData.password
+      // const data = {
+      //   email: formData.email,
+      //   password: formData.password
+      // }
 
-      }
-
-      this.http.post('http://localhost:8000/api/login', data).subscribe(
+      this.loginService.login(email, password).subscribe(
         (result: any) => {
           localStorage.setItem('token', result.token);
           localStorage.setItem('role', result.user.role);
-          if(localStorage.getItem('role') == '3') {
+          if(localStorage.getItem('role') == '2') {
             this.router.navigate(['admin/admin'])
           } 
-          else if (localStorage.getItem('id') == '2') {
+          else if (localStorage.getItem('role') == '3') {
             this.router.navigate(['admin/admin'])
           }
           
