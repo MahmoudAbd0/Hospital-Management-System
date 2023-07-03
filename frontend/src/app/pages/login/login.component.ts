99f7f6529ad1ee3778b5a6821d6e7b61e0f3f663
Yehia Nabil <yehn@outlook.com>
2023-07-03 17:05:45 +0300

    updated login

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -3,16 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-interface LoginResponce {
-  id: number;
-  username: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  gender: string;
-  image: string;
-  token: string;
-}
 
 @Component({
   selector: 'app-login',
@@ -39,21 +29,21 @@ export class LoginComponent implements OnInit {
       const formData = this.form.getRawValue();
 
       const data = {
-        username: formData.email,
+        email: formData.email,
         password: formData.password
 
       }
 
-      this.http.post<LoginResponce>('https://dummyjson.com/auth/login', data).subscribe(
-        result => { 
-          localStorage.setItem('token', result.token),
-          localStorage.setItem('id', result.id.toString())
-          if(localStorage.getItem('id') == '1') {
+      this.http.post('http://localhost:8000/api/login', data).subscribe(
+        (result: any) => {
+          localStorage.setItem('token', result.token);
+          localStorage.setItem('role', result.user.role);
+          if(localStorage.getItem('role') == '3') {
             this.router.navigate(['admin/admin'])
           } 
-          // else if (localStorage.getItem('id') == '2') {
-          //   this.router.navigate(['admin/admin'])
-          // }
+          else if (localStorage.getItem('id') == '2') {
+            this.router.navigate(['admin/admin'])
+          }
           
           
         }, error => {
