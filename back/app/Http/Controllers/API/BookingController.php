5f30a736e9375819ccc24e2084f0df87d8a349c9
yasmen-ahmed <yasmenahmedmuhamed@gmail.com>
2023-07-03 23:28:07 +0300

    git edit booking

--- back/app/Http/Controllers/API/BookingController.php
+++ back/app/Http/Controllers/API/BookingController.php
@@ -16,35 +16,21 @@ class BookingController extends Controller
      */
     public function index(Request $request)
     {
-     // Get the authenticated user
-     $user = $request->user();
+        $bookings = Booking::query();
 
-     if($user->role == '3') {
-        // Retrieve the bookings for the user
-        $bookings = Booking::where('user_id', $user->id)->get();
+        // Check if a user filter is present
+        if ($request->has('user_id')) {
+            $bookings->where('user_id', $request->user_id);
+        }
+
+        // Check if a room filter is present
+        if ($request->has('room_id')) {
+            $bookings->where('room_id', $request->room_id);
+        }
+
+        $bookings = $bookings->get();
 
         return response()->json(['bookings' => $bookings]);
-        
-    
-        // // Check if a room filter is present
-        // if ($request->has('room_id')) {
-        //     $bookings->where('room_id', $request->room_id);
-        // }
-
-     }
-
- 
-     // Get the bookings
-     $bookings = $bookings->get();
-
-
-
-        // $results = Booking::with('Room')
-        // ->get();
-
-        // return response()->json($results);
-
-       
     }
 
     /**
@@ -53,57 +39,32 @@ class BookingController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(BookingRequest $request)
     {
-        // Get the authenticated user
-    // $user = $request->user();
-    
-    // Check if the user has the "patient" role
-    if (!$user->hasRole('patient')) {
-        return response()->json(['error' => 'Unauthorized'], 401);
-    }
-    
-    // Get the input data from the request
-    $roomId = $request->input('room_id');
-    
-    
-    // Get the selected room
-    $room = Room::find($roomId);
-    
-    if (!$room) {
-        return response()->json(['error' => 'Room not found'], 404);
-    }
-    
-    // Create a new booking
-    $booking = new Booking();
-    $booking->user_id = $user->id;
-    $booking->room_id = $room->id;
-    $booking->date = Carbon::now();
-    $booking->save();
-    
-    // Return a success response
-    // return response()->json(['success' => true]);
-    return response()->json($booking);
-    }
+        $user = User::findorFail($request->user_id);
 
+        if($user->role !== 3) {
+            return response()->json(['error' => 'User is not a patient']);
+        }
+
+        $booking = Booking::create($request->validated());
+
+        return response()->json(['booking' => $booking]);
+    }
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Booking  $booking
      * @return \Illuminate\Http\Response
      */
-    public function show($user_id, $room_id)
-{
-    $booking = Booking::where('user_id', $user_id)
-                      ->where('room_id', $room_id)
-                      ->first();
-    
-    if ($booking) {
-        return response()->json(['booking' => $booking]);
-    } else {
-        return response()->json(['message' => 'Booking not found.'], 404);
+    public function show(Booking $booking)
+    {
+        if ($booking) {
+            return response()->json(['booking' => $booking]);
+        } else {
+            return response()->json(['message' => 'Booking not found.'], 404);
+        }
     }
-}
 
     /**
      * Update the specified resource in storage.
@@ -112,19 +73,17 @@ class BookingController extends Controller
      * @param  \App\Models\Booking  $booking
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $user_id, $room_id)
+    public function update(BookingRequest $request, Booking $booking)
     {
-        $booking = Booking::where('user_id', $user_id)
-        ->where('room_id', $room_id)
-        ->first();
+        $user = User::findorFail($request->user_id);
 
-            if ($booking) {
-            $booking->update($request->all());
+        if($user->role !== 3) {
+            return response()->json(['error' => 'User is not a patient']);
+        }
 
-            return response()->json(['message' => 'Booking updated successfully.']);
-            } else {
-            return response()->json(['message' => 'Booking not found.'], 404);
-            }
+        $booking->update($request->validated());
+
+        return response()->json(['booking' => $booking]);
     }
 
     /**
@@ -133,17 +92,13 @@ class BookingController extends Controller
      * @param  \App\Models\Booking  $booking
      * @return \Illuminate\Http\Response
      */
-    public function destroy($user_id, $room_id)
+    public function destroy(Booking $booking)
     {
-        $booking = Booking::where('user_id', $user_id)
-                      ->where('room_id', $room_id)
-                      ->first();
-    
-    if ($booking) {
-        $booking->delete();
-        return response()->json(['message' => 'Booking deleted successfully.']);
-    } else {
-        return response()->json(['message' => 'Booking not found.'], 404);
-    }
+        if ($booking) {
+            $booking->delete();
+            return response()->json(['message' => 'Booking deleted']);
+        } else {
+            return response()->json(['message' => 'Booking not found.'], 404);
+        }
     }
 }
